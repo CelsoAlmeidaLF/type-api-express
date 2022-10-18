699a5fa1bc50e1bf21dcb9b56879b0c52ec4b853
CelsoAlmeida <celso.almeida.leite@hotmail.com>
2022-10-18 18:22:41 -0300

    fase1: create module telegram.

---
+++ src/modules/telegram.ts
@@ -0,0 +1,28 @@
+
+import TelegramBot from "node-telegram-bot-api";
+import { format } from 'date-fns';
+
+export class Telegram {
+
+        private token: string
+        private chatId: string
+
+        constructor(){
+            this.token = '...';
+            this.chatId = '...';
+        }
+
+        sendMessage(msg: string){
+
+            try{        
+                let date = `${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`; 
+                let bot = new TelegramBot(this.token);
+                bot.sendMessage(this.chatId, `[${date}] ${msg}`);
+                console.log('enviado, telegram ...');
+            }catch(ex){
+                throw ex;
+            }
+
+       }
+
+}
