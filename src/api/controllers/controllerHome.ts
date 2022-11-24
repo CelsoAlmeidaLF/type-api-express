bb10631f06f4ec23695523706fa24aad49903d8e
CelsoAlmeida <celso.almeida.leite@hotmail.com>
2022-11-24 08:42:10 -0300

    create: authorization 1* part.

--- src/api/controllers/controllerHome.ts
+++ src/api/controllers/controllerHome.ts
@@ -1,11 +1,32 @@
-
 import { Response, Request } from 'express';
 import API from '../util';
 
 export default class Home { 
 
     static async index(req: Request, res: Response) {
-        API.Ok(res, 'mu API');
+        
+        let cookie = req.headers.cookie
+        let jwt = req.headers.authorization;
+
+        try{
+            if(jwt !== undefined){
+                if(cookie !== undefined){
+                    API.Ok(res, { message: "token validado!"});
+                }
+                else{
+                    res.cookie('token', jwt, { maxAge: 10000 });
+                    API.Ok(res, { message: "token validado!"});
+                }
+            }else{
+                if(cookie === undefined){
+                    console.log('[not authorization: 400]');
+                    API.Fail(res, { message: "not authorization"});
+                }
+            }
+        }catch(err){
+            API.Fail(res, { message: "not authorization", error: err });
+        }
+
     }   
 
 }
--- src/api/middleware/handler.ts
+++ src/api/middleware/handler.ts
@@ -1,10 +1,12 @@
 import { RequestHandler } from "express";
 import parser from 'body-parser';
 import { BodyParser } from "body-parser";
+import cookieParser from 'cookie-parser'
 
 let handler: RequestHandler
 
 handler = parser.urlencoded({extended: true})
 handler = parser.json()
+//handler = cookieParser()
 
 export default handler
